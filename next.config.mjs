964a61e0cb9eb4c64fcba76e1e5a14d8ca63e036
南川 <xingjian@baichuan-inc.com>
2023-10-22 20:51:40 +0800

    added tabs

--- next.config.mjs
+++ next.config.mjs
@@ -4,36 +4,38 @@ import {withContentlayer} from 'next-contentlayer'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    // pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  // pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 
-    reactStrictMode: true,
-    experimental: {
-        appDir: true,
+  reactStrictMode: true,
+  experimental: {
+    esmExternals: false,
 
-        // mdxRs: true, // ref: https://nextjs.org/docs/app/building-your-application/configuring/mdx
-    },
-
-    distDir: process.env.DIST ?? ".next",
-
-    // ref: https://nextjs.org/docs/api-reference/next/image#remote-patterns
-    images: {
-        remotePatterns: [// ref:https://stackoverflow.com/a/73951135/9422455
-            {protocol: "http", hostname: "**"},
-            {protocol: "https", hostname: "**"}
-        ]
-    },
+    // mdxRs: true, // ref: https://nextjs.org/docs/app/building-your-application/configuring/mdx
+  },
 
 
-    // swcMinify: true, // ref: https://contentlayer.dev/docs/getting-started-cddd76b7
 
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.node/,
-            use: 'raw-loader',
-        });
+  distDir: process.env.DIST ?? ".next",
 
-        return config;
-    },
+  // ref: https://nextjs.org/docs/api-reference/next/image#remote-patterns
+  images: {
+    remotePatterns: [// ref:https://stackoverflow.com/a/73951135/9422455
+      {protocol: "http", hostname: "**"},
+      {protocol: "https", hostname: "**"}
+    ]
+  },
+
+
+  // swcMinify: true, // ref: https://contentlayer.dev/docs/getting-started-cddd76b7
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: 'raw-loader',
+    });
+
+    return config;
+  },
 }
 
 export default withContentlayer(nextConfig)
